201ab470c40f568f7deb5fb4a4b42aebbd8b5960
iamsanamrai <lonelykan6a@gmail.com>
2024-02-08 23:16:11 +0530

    function for next button to change the another questions

---
+++ script.js
@@ -0,0 +1,10 @@
+//display the questions
+const questionsDiv = document.getElementById("ques")
+questionsDiv.innerHTML = "1.What is the name of the world's largest ocean?"
+
+//next question button
+function next()  {
+    const paragraph = document.getElementById("ques")
+    paragraph.innerHTML = "2.What is the capital of the Nepal?"
+    
+}
